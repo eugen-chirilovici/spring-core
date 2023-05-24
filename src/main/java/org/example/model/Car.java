f12a5c29588e3f7226405cdf493ed6501d7ef4e2
eugenchirilovici <echirilovici123endava@gmail.com>
2023-05-24 11:33:05 +0300

    @Autowiring

--- src/main/java/org/example/model/Car.java
+++ src/main/java/org/example/model/Car.java
@@ -1,17 +1,16 @@
 package org.example.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Car {
 
+    @Autowired
     private Engineer engineer;
-
-    public Car(Engineer engineer) {
-        this.engineer = engineer;
-    }
 
     public Engineer getEngineer() {
         return engineer;
     }
+
 }
